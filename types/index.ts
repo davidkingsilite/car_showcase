72c4c3ab1b20e01e0204932d080e0c12d1685bde
davidkingsilite <davidnwachukwu3@gmail.com>
2024-09-02 21:53:48 +0100

    initial commit

---
+++ types/index.ts
@@ -0,0 +1,28 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: string;
+    btnType?: "button" | "submit" ;
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer:string;
+    setManufacturer: (manufacture: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
